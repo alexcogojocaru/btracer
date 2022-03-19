190c8fb514914d4044dab7e6400288c7dafa5131
alexcogojocaru <alex.cojocaru73@gmail.com>
2022-03-19 21:20:53 +0200

    request wrapper

--- examples/simple-communication/microservice1/main.go
+++ examples/simple-communication/microservice1/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
 
-	bee "github.com/alexcogojocaru/btracer/exporters/bee"
+	"github.com/alexcogojocaru/btracer/exporters/bee"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/sdk/trace"
+
+	bhttp "github.com/alexcogojocaru/btracer/http"
 )
 
 func main() {
@@ -11,4 +16,24 @@ func main() {
 	beeExporter, _ := bee.NewBeeExporter(&agentConfig)
 
 	log.Print(beeExporter)
+
+	traceProvider := trace.NewTracerProvider(trace.WithBatcher(beeExporter))
+	defer func() {
+		if err := traceProvider.Shutdown(context.Background()); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	otel.SetTracerProvider(traceProvider)
+	tracer := otel.Tracer("BTracer")
+
+	otelCtx, span := tracer.Start(context.Background(), "Main")
+	defer span.End()
+
+	resp, err := bhttp.Request("http://localhost:8090/ping", otelCtx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print(resp)
 }
